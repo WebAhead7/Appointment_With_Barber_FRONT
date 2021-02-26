c02096d23a46220d796683e81528a8ebea98579c
Muhammad Awwad <60406664+muhammadawwad9@users.noreply.github.com>
2021-02-26 02:06:13 +0200

    Update api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -2,9 +2,8 @@ import React from "react";
 
 const api = (url, options) => {
   const server = `http://localhost:4000/`;
-  const heroku = "https://barber-appointment-backend.herokuapp.com/";
 
-  return fetch(`${heroku}${url}`, {
+  return fetch(`${server}${url}`, {
     ...options,
   })
     .then((response) => {
